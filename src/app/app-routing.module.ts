1878bc4090e74eab480c94078c2c229c25a5ff2d
Diego Fernandez <diego.fernandez.58@gmail.com>
2019-07-02 08:45:14 -0600

    Primer Commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,9 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', loadChildren: './home/home.module#HomePageModule' },
+  { path: 'register', loadChildren: './register/register.module#RegisterPageModule' },
+  { path: 'login', loadChildren: './login/login.module#LoginPageModule' },
+  { path: 'admin', loadChildren: './admin/admin.module#AdminPageModule' },
 ];
 
 @NgModule({
